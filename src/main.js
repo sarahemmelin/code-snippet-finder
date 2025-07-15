f70c1653a38eac11a3d04ea04a77b971939d8be8
sarahemmelin <sarahemmelin@gmail.com>
2025-07-15 21:09:43 +0200

    Har lagt til minimum og maksimum vidde på left-sidebar.

--- src/main.js
+++ src/main.js
@@ -9,7 +9,18 @@ function onMouseDown(e) {
 };
 
 function onMouseMove(e) {
-  const newWidth = e.clientX;
+  const minWidth = 150;
+  const maxWidth = 500;
+  let newWidth = e.clientX;
+
+  if (newWidth < minWidth) {
+    newWidth = minWidth;
+  };
+
+  if (newWidth > maxWidth) {
+    newWidth = maxWidth;
+  }
+
   document.querySelector('.left-sidebar').style.width = `${newWidth}px`;
 };
 
